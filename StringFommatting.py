d398724bff23a16b26eeb5a1c2eedb487c7ac814
CHIRAGGARG1697 <chiraggarg.0106@gmail.com>
2024-08-12 15:55:00 +0530

    First Commit

---
+++ StringFommatting.py
@@ -0,0 +1,17 @@
+name = 'john'
+print("Hello,%s!"% name)
+name = 'Chirag'
+age =  27
+print("%s is %d years old"%(name,age))
+mylist = [1,2,3]
+print("A list is: %s"% mylist)
+print("Single quotes are ' ' ")
+print('Double quotes are " " ')
+asString = "Chirag Garg"
+print(asString.index("r"))
+print(asString.count("g"))
+print(len(asString))
+print(asString[1:9])
+print(asString.upper())
+print(asString.lower())
+print(asString[::-1])  
